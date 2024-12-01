f4f708c72436de6bcdb2c0c0d590dd8b4babdd5a
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-01 22:54:31 +0300

    Corrected the reaction Corrected the handling of variables Clarified the variable name Expanded if, made the code flat

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    dvmn_token = os.getenv('DVMN_TOKEN')
-    bot_token = os.getenv('BOT_TOKEN')
-    chat_id = os.getenv('CHAT_ID')
+    dvmn_token = os.environ['DVMN_TOKEN']
+    bot_token = os.environ['BOT_TOKEN']
+    chat_id = os.environ['CHAT_ID']
     bot = telegram.Bot(token=bot_token)
-    seconds = 5
+    number = 5
 
     url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': dvmn_token,}
@@ -21,29 +21,29 @@ def main():
 
     while True:
         try:
-            response = requests.get(url, headers=headers, params=payload, timeout=seconds)
+            response = requests.get(url, headers=headers, params=payload, timeout=number)
             response.raise_for_status()
             format_response = response.json()
             if 'last_attempt_timestamp' in format_response:
                 payload['timestamp'] = format_response['last_attempt_timestamp']
-            if format_response.get('status') == 'found':
                 print('Найдены новые события!')
                 for attempt in format_response.get('new_attempts', []):
                     pprint(attempt)
-                    if attempt['is_negative']:
-                        bot.send_message(chat_id=chat_id,
-                                         text=f'''У вас проверили работу "{attempt['lesson_title']}"
-К сожалению, в работе нашлись ошибки.
-{attempt['lesson_url']}''')
-                    else:
+                    if not attempt['is_negative']:
                         bot.send_message(chat_id=chat_id,
                                          text=f'''У вас проверили работу "{attempt['lesson_title']}"
 Преподавателю всё понравилось, можно приступать к следующему уроку!''')
+                        continue
+                    bot.send_message(chat_id=chat_id,
+                                         text=f'''У вас проверили работу "{attempt['lesson_title']}"
+К сожалению, в работе нашлись ошибки.
+{attempt['lesson_url']}''')
+                    # else:
+
         except requests.exceptions.ConnectionError:
             print('Соединение прервано. Скрипт продолжает работу', file=sys.stderr)
             time.sleep(1800)
-        except requests.exceptions.ReadTimeout as error:
-            print(f'Прошло {seconds} сек. Продолжаем работу - ошибка ({error})')
+        except requests.exceptions.ReadTimeout:
             continue
 
 
