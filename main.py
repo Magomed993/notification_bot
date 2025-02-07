ca6a1fd5bc52ba3dd57ef83e861acf570ca775cc
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2025-02-07 11:06:18 +0300

    Send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -2,18 +2,40 @@ import requests
 import time
 import sys
 import os
+import logging
 import telegram
 from pprint import pprint
 from dotenv import load_dotenv
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def main():
     load_dotenv()
     dvmn_token = os.environ['DVMN_TOKEN']
     bot_token = os.environ['BOT_TOKEN']
+    loggs_token = os.environ['LOGGS_TOKEN']
     chat_id = os.environ['CHAT_ID']
     bot = telegram.Bot(token=bot_token)
+    log_bot = telegram.Bot(token=loggs_token)
     timeout = 5
+
+    logger = logging.getLogger('dvmn_logger')
+    logger.setLevel(logging.INFO)
+    log_handler = TelegramLogsHandler(log_bot, chat_id)
+    logger.addHandler(log_handler)
+
+    logger.info("Бот запущен!")
 
     url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': dvmn_token,}
